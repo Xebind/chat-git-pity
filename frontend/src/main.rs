345f821e46b41f30ffc7a4ecd4bc13056a0ed29a
Xebind <xebind@gmail.com>
2023-04-22 07:29:42 +0200

    simple frontend using axum

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,24 +1,66 @@
-use yew::prelude::*;
+use axum::{
+    routing::get,
+    body::{boxed, Body, BoxBody},
+    http::{Request, Response, StatusCode, Uri},
+    Router,
+};
+use std::net::SocketAddr;
+use tower::ServiceExt;
+use tower_http::services::ServeDir;
 
-#[function_component]
-fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
+#[tokio::main]
+async fn main() {
+    // Route all requests on "/" endpoint to anonymous handler.
+    //
+    // A handler is an async function which returns something that implements
+    // `axum::response::IntoResponse`.
+
+    // A closure or a function can be used as handler.
+
+    let app = Router::new()
+                    .route("/healthcheck", get(healthcheck))
+                    .nest_service("/", get(handler));
+    //        Router::new().route("/", get(|| async { "Hello, world!" }));
+
+    // Address that server will bind to.
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+
+    // Use `hyper::server::Server` which is re-exported through `axum::Server` to serve the app.
+    axum::Server::bind(&addr)
+        // Hyper server takes a make service.
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn healthcheck() -> &'static str {
+    "healthcheck: OK"
+}
+
+async fn handler(uri: Uri) -> Result<Response<BoxBody>, (StatusCode, String)> {
+    let res = get_static_file(uri.clone()).await?;
+
+    if res.status() == StatusCode::NOT_FOUND {
+        // try with `.html`
+        // TODO: handle if the Uri has query parameters
+        match format!("index.html").parse() {
+            Ok(uri_html) => get_static_file(uri_html).await,
+            Err(_) => Err((StatusCode::INTERNAL_SERVER_ERROR, "Invalid URI".to_string())),
         }
-    };
-
-    html! {
-        <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
-        </div>
+    } else {
+        Ok(res)
     }
 }
 
-fn main() {
-    yew::Renderer::<App>::new().render();
+async fn get_static_file(uri: Uri) -> Result<Response<BoxBody>, (StatusCode, String)> {
+    let req = Request::builder().uri(uri).body(Body::empty()).unwrap();
+
+    // `ServeDir` implements `tower::Service` so we can call it with `tower::ServiceExt::oneshot`
+    match ServeDir::new(".").oneshot(req).await {
+        Ok(res) => Ok(res.map(boxed)),
+        Err(err) => Err((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Something went wrong: {}", err),
+        )),
+    }
 }
